d79cea7aebf2e9d553f25629e1326471f26e5d3d
Jaremy Creechley <creechley@gmail.com>
2025-12-05 23:18:15 -0700

    revert back to claude walk version

--- examples/stackwalk_amd64_excp.nim
+++ examples/stackwalk_amd64_excp.nim
@@ -17,12 +17,12 @@ template mkDeep(procName, nextName: untyped) =
     inc depthSink
 
 mkDeep(deep1, deep0)
-mkDeep(deep2, deep1)
-mkDeep(deep3, deep2)
-mkDeep(deep4, deep3)
-mkDeep(deep5, deep4)
-mkDeep(deep6, deep5)
-mkDeep(deep7, deep6)
+#mkDeep(deep2, deep1)
+#mkDeep(deep3, deep2)
+#mkDeep(deep4, deep3)
+#mkDeep(deep5, deep4)
+#mkDeep(deep6, deep5)
+mkDeep(deep7, deep1)
 
 #proc deep1() {.noinline.} = deep0()
 #proc deep2() {.noinline.} = deep1()
